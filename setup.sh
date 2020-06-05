98cac636ac229fce83aeedfe768e9654a4c84590
Sam Smith <sam@samsmith.me>
2020-06-05 09:44:03 +0100

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ apt-get update
 apt-get install net-tools -y
 apt-get install apache2 -y
 apt-get install curl -y
-apt-get install mariadb-server -y
-apt-get install php libapache2-mod-php php-mysql php-cli php-mbstring php-bz2 php-zip php-xdebug php-curl -y
+apt-get install php libapache2-mod-php php-mysql php-cli php-mbstring php-bz2 php-zip php-xdebug php-curl php-xml php-dom php-simplexml -y
 apt-get install curl git unzip -y
+apt-get install mariadb-server -y
 
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
@@ -35,16 +35,6 @@ ufw allow 80
 ufw allow 443
 ufw reload
 
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip
-unzip phpMyAdmin-5.0.2-all-languages.zip -d /var/www/html
-mv /var/www/html/phpMyAdmin-5.0.2-all-languages/ /var/www/html/phpMyAdmin
-cp /media/share/config/config.inc.php /var/www/html/phpMyAdmin
-mkdir /var/www/html/phpMyAdmin/tmp
-chmod 775 /var/www/html/phpMyAdmin/tmp/
-chown -R root:www-data /var/www/html
-rm phpMyAdmin-5.0.2-all-languages.zip
-rm /var/www/html/index.html
-
 touch /var/www/html/links.html
 echo '<p><a href="//astroid/phpMyAdmin/"><img src="/phpMyAdmin/favicon.ico" style="vertical-align:middle;padding-right:1em;">phpMyAdmin</a></p>' >> /var/www/html/links.html
 sed -i '/DocumentRoot \/var\/www\/html/r /media/share/config/apacheDirectoryConfig.txt' /etc/apache2/sites-available/000-default.conf
@@ -55,3 +45,13 @@ mysqladmin password "root"
 echo "DELETE FROM mysql.user WHERE User='';" | mysql -uroot
 echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql -uroot
 echo "FLUSH PRIVILEGES;" | mysql -uroot
+
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip
+unzip phpMyAdmin-5.0.2-all-languages.zip -d /var/www/html
+mv /var/www/html/phpMyAdmin-5.0.2-all-languages/ /var/www/html/phpMyAdmin
+cp /media/share/config/config.inc.php /var/www/html/phpMyAdmin
+mkdir /var/www/html/phpMyAdmin/tmp
+chmod 775 /var/www/html/phpMyAdmin/tmp/
+chown -R root:www-data /var/www/html
+rm phpMyAdmin-5.0.2-all-languages.zip
+rm /var/www/html/index.html
